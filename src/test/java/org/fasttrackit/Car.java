0fa7829509869492282b728282a368c94ef13cd6
razvanrosa <razvan_rosa@yahoo.com>
2020-05-27 11:34:14 +0300

    First commit

---
+++ src/test/java/org/fasttrackit/Car.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String make;
+    String model;
+    String color;
+    double mileage;
+    double fuelLevel;
+    double maxSpeed;
+    double totalTraveledDistance;
+    boolean damaged;
+    int doorCount;
+}
