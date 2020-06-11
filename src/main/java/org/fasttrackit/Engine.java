a43fc4c5ded0f4b46ceead252238908e28fdd3d1
razvanrosa <razvan_rosa@yahoo.com>
2020-06-11 20:26:15 +0300

    Added static method usage example

--- src/main/java/org/fasttrackit/Engine.java
+++ src/main/java/org/fasttrackit/Engine.java
@@ -1,8 +1,11 @@
 package org.fasttrackit;
 
+import java.time.LocalDate;
+
 public class Engine {
 
     String manufacturer;
     int capacity;
+    LocalDate manufacturingDate = LocalDate.now();
 
 }
