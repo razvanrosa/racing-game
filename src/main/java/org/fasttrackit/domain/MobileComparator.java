eadf9cf3fa90139ce79e95d65019e8f25a531dfa
razvanrosa <razvan_rosa@yahoo.com>
2020-07-09 18:46:04 +0300

    handled null values in mobile comparator

--- src/main/java/org/fasttrackit/domain/MobileComparator.java
+++ src/main/java/org/fasttrackit/domain/MobileComparator.java
@@ -6,7 +6,16 @@ public class MobileComparator implements Comparator<Mobile> {
 
     @Override
     public int compare(Mobile o1, Mobile o2) {
-        //todo: handle null values
+        //both variables are null or contains references to the same object
+        if(o1 ==o2){
+            return 0;
+        }
+        if(o1 == null){
+            return -1;
+        }
+        if(o2 == null){
+            return 1;
+        }
         return Double.compare(o1.getTotalTraveledDistance(), o2.getTotalTraveledDistance());
 
     }
